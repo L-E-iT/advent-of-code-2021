aa3aaf6d809fd25e16e6c40e735e2dc845a87f93
Brian Elliott <ellio2bj@dukes.jmu.edu>
2021-12-07 23:55:26 -0500

    day 6

---
+++ challenges/day-6/problem-2/solution.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+file_data = map(int, open("solution-data.txt").read().split(","))
+
+
+def solve(fish_data, days):
+    new_fish_data = fish_data.copy()
+    for i in range(days):
+        new_fish_data = simulate_day_growth(new_fish_data)
+    return new_fish_data
+
+
+def simulate_day_growth(lantern_fish):
+    new_fish_list = {i: 0 for i in range(0, 9)}
+    for id, lantern_fish in lantern_fish.items():
+        if id == 0:
+            new_fish_list[6] += lantern_fish
+            new_fish_list[8] += lantern_fish
+        else:
+            new_fish_list[id-1] += lantern_fish
+    return new_fish_list
+
+
+def generate_lanternfish(data):
+    initial_lantern_fish_list = {i: 0 for i in range(0, 8)}
+    fish_data = Counter(data)
+    for uid, lantern_fish in initial_lantern_fish_list.items():
+        initial_lantern_fish_list[uid] += fish_data[uid] or 0
+    return initial_lantern_fish_list
+
+
+print(sum(solve(generate_lanternfish(file_data), 256).values()))
---
+++ challenges/day-6/problem-2/test.py
@@ -0,0 +1,29 @@
+import unittest
+from solution import generate_lanternfish, simulate_day_growth, solve
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.data = map(int, open("test-data.txt").read().split(","))
+
+    def test_generate_lanternfist(self):
+        fish_list = generate_lanternfish(self.data)
+        self.assertEqual(fish_list[3], 2)
+
+    def test_simulate_day(self):
+        fish_list = generate_lanternfish(self.data)
+        one_day_fish_list = simulate_day_growth(fish_list)
+        two_day_fish_list = simulate_day_growth(one_day_fish_list)
+        self.assertEqual(one_day_fish_list[2], 2)
+        print(one_day_fish_list)
+        print(two_day_fish_list)
+        self.assertEqual(two_day_fish_list[8], 1)
+
+    def test_day_growth_count(self):
+        fish_list = generate_lanternfish(self.data)
+        days18 = solve(fish_list, 18)
+        days80 = solve(fish_list, 80)
+        days256 = solve(fish_list, 256)
+        self.assertEqual(sum(days18.values()), 26)
+        self.assertEqual(sum(days80.values()), 5934)
+        self.assertEqual(sum(days256.values()), 26984457539)
